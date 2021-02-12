7ab42d63cbc19728973a1594a295bdeb0f102545
MaxumSmal37 <40183260+MaxumSmal37@users.noreply.github.com>
2021-02-12 14:20:28 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const on = document.querySelector('.btn-1')
+const stope = document.querySelector('.btn-2')
+
+
+
+const rest = () =>
+{
+    location.href = 'https://robotchip.ru/obzor-modulya-osveshchennosti-lm393/';
+
+}
+
+stope.addEventListener('click', rest);
